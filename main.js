f0ea4cff5999917bfad3478ac2de35b33f42c9a6
TurNyak <githybtyrnyak@gmail.com>
2023-02-05 00:27:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 console.log(localStorage.getItem('top20'))
 var recordMain = localStorage.getItem('top20');
+var to=null;
+localStorage.setItem('to',to)
+function lert() {
+    if(localStorage.getItem('money')>30){
+        to--;
+        localStorage.setItem('to',to)
+    }
+}
