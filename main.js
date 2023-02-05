dc10f6e0aace391a4074b0d2286a2c5f2b788c82
TurNyak <githybtyrnyak@gmail.com>
2023-02-05 12:15:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 console.log(localStorage.getItem('top20'))
 var recordMain = localStorage.getItem('top20');
 var to=null;
-localStorage.setItem('to',to)
+sessionStorage.setItem('to',to)
 function img1() {
-    if(localStorage.getItem('money')>=30){
-        to--;
-        localStorage.setItem('to',to)
-        var img2 = document.querySelector("img.img");
-        img2.className = "img2";
-    }
+        if(localStorage.getItem('money')>=30){
+            to++;
+            sessionStorage.setItem('to',to)
+            var img2 = document.querySelector("img.img");
+            img2.className = "img2";
+            
+        }
+}
+function img2() {
+        if(localStorage.getItem('money')>=30){
+            to++;
+            sessionStorage.setItem('to',to)
+            var img2 = document.querySelector("img.img");
+            img2.className = "img2";
+            
+        }
 }
