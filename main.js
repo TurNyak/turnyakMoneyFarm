875d631879345bcca4b10beb1323a84dc0fcf33d
TurNyak <githybtyrnyak@gmail.com>
2023-02-05 14:14:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,7 @@ function img1() {
             
         }
 }
-function img2() {
-        if(localStorage.getItem('money')>=30){
-            to++;
-            sessionStorage.setItem('to',to)
-            var img2 = document.querySelector("img.img");
-            img2.className = "img2";
-            
-        }
+function m1(){
+    document.getElementById('m1').style = 'height: 40px;width: 40px;animation: moneybags 0.4s linear infinite;';
+    console.log("sigbasigbibga")
 }
