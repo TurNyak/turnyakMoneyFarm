cb6a4e55930c229aa4d1b0b259c3c4555b4113b3
TurNyak <githybtyrnyak@gmail.com>
2023-02-08 21:38:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 console.log(localStorage.getItem('top20'))
-var recordMain = localStorage.getItem('top20');
-var to=null;
-sessionStorage.setItem('to',to)
-function img1() {
-        if(localStorage.getItem('money')>=30){
-            to++;
-            sessionStorage.setItem('to',to)
-            var img2 = document.querySelector("img.img");
-            img2.className = "img2";
-            
-        }
+var recordMain = localStorage.getItem('top20')
+
+let record = 0
+let money = 0
+let theSalary = 1
+
+var loader1 = 0
+function loader() {
+	if (loader1 == 0) {
+		if (localStorage.getItem('money') >= 50) {
+			loader1++
+			document.getElementById('loader').style =
+				'height: 40px;width: 40px;animation:loaderAnimation 0.4s linear infinite;'
+			money -= 50
+			theSalary++
+		}
+	}
 }
-function m1(){
-    document.getElementById('m1').style = 'height: 40px;width: 40px;animation: moneybags 0.4s linear infinite;';
-    console.log("sigbasigbibga")
+
+const abuttons = document.getElementsByTagName('button')
+
+console.log(sessionStorage.getItem('to'))
+function makeCounter() {
+	var pol
+	for (let i = 0; i < abuttons.length; i++) {
+		let btn = abuttons[i]
+		btn.onclick = function () {
+			;(money += theSalary) & (record += theSalary)
+			;(document.getElementById('money').innerHTML = 'Money :' + money) &
+				(document.getElementById('record').innerHTML = 'Record :' + record)
+			localStorage.setItem('money', money)
+			if (money < 0) {
+				money += 30
+			}
+			if (record > localStorage.getItem('top20')) {
+				pol = record
+				localStorage.setItem('top20', pol)
+			}
+			console.log(pol)
+		}
+	}
 }
